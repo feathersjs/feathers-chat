918111a4296ffb32980a9d3eec53b4b6c3a98b99
daffl <daff@neyeon.com>
2023-01-03 16:16:52 -0800

    Finalize quick start

--- quick-start/app.ts
+++ quick-start/app.ts
@@ -1,78 +1,70 @@
-import { feathers } from "@feathersjs/feathers";
-import {
-  koa,
-  rest,
-  bodyParser,
-  errorHandler,
-  serveStatic,
-} from "@feathersjs/koa";
-import socketio from "@feathersjs/socketio";
+import { feathers } from '@feathersjs/feathers'
+import { koa, rest, bodyParser, errorHandler, serveStatic } from '@feathersjs/koa'
+import socketio from '@feathersjs/socketio'
 
 // This is the interface for the message data
 interface Message {
-  id?: number;
-  text: string;
+  id?: number
+  text: string
 }
 
 // A messages service that allows us to create new
 // and return all existing messages
 class MessageService {
-  messages: Message[] = [];
+  messages: Message[] = []
 
   async find() {
     // Just return all our messages
-    return this.messages;
+    return this.messages
   }
 
-  async create(data: Pick<Message, "text">) {
+  async create(data: Pick<Message, 'text'>) {
     // The new message is the data text with a unique identifier added
     // using the messages length since it changes whenever we add one
     const message: Message = {
       id: this.messages.length,
-      text: data.text,
-    };
+      text: data.text
+    }
 
     // Add new message to the list
-    this.messages.push(message);
+    this.messages.push(message)
 
-    return message;
+    return message
   }
 }
 
 // This tells TypeScript what services we are registering
 type ServiceTypes = {
-  messages: MessageService;
-};
+  messages: MessageService
+}
 
 // Creates an ExpressJS compatible Feathers application
-const app = koa<ServiceTypes>(feathers());
+const app = koa<ServiceTypes>(feathers())
 
 // Use the current folder for static file hosting
-app.use(serveStatic("."));
+app.use(serveStatic('.'))
 // Register the error handle
-app.use(errorHandler());
+app.use(errorHandler())
 // Parse JSON request bodies
-app.use(bodyParser());
+app.use(bodyParser())
 
 // Register REST service handler
-app.configure(rest());
+app.configure(rest())
 // Configure Socket.io real-time APIs
-app.configure(socketio());
+app.configure(socketio())
 // Register our messages service
-app.use("messages", new MessageService());
+app.use('messages', new MessageService())
 
 // Add any new real-time connection to the `everybody` channel
-app.on("connection", (connection) => app.channel("everybody").join(connection));
+app.on('connection', (connection) => app.channel('everybody').join(connection))
 // Publish all events to the `everybody` channel
-app.publish((_data) => app.channel("everybody"));
+app.publish((_data) => app.channel('everybody'))
 
 // Start the server
-app
-  .listen(3030)
-  .then(() => console.log("Feathers server listening on localhost:3030"));
+app.listen(3030).then(() => console.log('Feathers server listening on localhost:3030'))
 
 // For good measure let's create a message
 // So our API doesn't look so empty
-app.service("messages").create({
-  text: "Hello world from the server",
-});
+app.service('messages').create({
+  text: 'Hello world from the server'
+})
